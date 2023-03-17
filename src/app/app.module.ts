781988da848dc9978c415d813866f6cc6f788251
diambere.cisse <diambere.cisse@outlook.com>
2023-03-17 10:59:18 +0100

    [STYLE][01] Create header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { AngularEmojisModule } from 'angular-emojis';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
